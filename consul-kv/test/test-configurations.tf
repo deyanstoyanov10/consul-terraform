339a7716eb82a1e3fa2735f6fceecc523d628f18
Deyan Stoyanov <didko985@gmail.com>
2023-05-13 19:38:16 +0300

    Refactor

--- consul-kv/test/test-configurations.tf
+++ consul-kv/test/test-configurations.tf
@@ -6,7 +6,7 @@ locals {
   microservices_configurations = flatten([
     for microservice in local.microservices : [
       for file in fileset("${path.module}/microservices/${microservice}", "*.json") : {
-        microservice_key_name = replace(file, ".json$", "")
+        microservice_key_name = replace(file, ".json", "")
         microservice_key_prefix = "${var.unit}/${microservice}"
         microservice_configuration = file("${path.module}/microservices/${microservice}/${file}")
       }
@@ -14,7 +14,7 @@ locals {
   ])
 }
 
-module "ms1_values" {
+module "test_unit_configurations" {
   source = "git::https://github.com/deyanstoyanov10/consul-kv-module.git?ref=v0.0.1"
 
   for_each = { for entry in local.microservices_configurations : "${entry.microservice_key_name}.${entry.microservice_key_prefix}.${entry.microservice_configuration}" => entry }

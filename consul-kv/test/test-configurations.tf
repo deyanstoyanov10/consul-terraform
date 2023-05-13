66caf0af01968c73cfa2103dd65245b2fe8f360c
Deyan Stoyanov <didko985@gmail.com>
2023-05-13 19:28:53 +0300

    Test logic

--- consul-kv/test/test-configurations.tf
+++ consul-kv/test/test-configurations.tf
@@ -1,13 +1,25 @@
 locals {
-  ms1_configurations = fileset("${path.module}/microservices/ms1", "*.json")
+  microservices = [
+    "ms1"
+  ]
+
+  microservices_configurations = flatten([
+    for microservice in local.microservices : [
+      for file in fileset("${path.module}/microservices/${microservice}", "*.json") : {
+        microservice_key_name = replace(file, ".json$", "")
+        microservice_key_prefix = "${var.unit}/${microservice}/"
+        microservice_configuration = file("${path.module}/microservices/${microservice}/${file}")
+      }
+    ]
+  ])
 }
 
 module "ms1_values" {
   source = "git::https://github.com/deyanstoyanov10/consul-kv-module.git?ref=v0.0.1"
 
-  for_each = { for file in local.ms1_configurations : file => file("${path.module}/microservices/ms1/${file}") }
+  for_each = local.microservices_configurations
 
-  datacenter = "dev"
-  key_path = "test/ms1/${each.key}"
-  key_value = each.value
+  datacenter = "${var.dc}"
+  key_path = "${each.microservice_key_prefix}${each.microservice_key_name}"
+  key_value = each.microservice_configuration
 }
